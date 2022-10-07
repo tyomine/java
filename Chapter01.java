33ae83b53ec86ba64beea58f09b50849bfcae214
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-07 13:27:53 +0000

    基礎練習

---
+++ Chapter01.java
@@ -0,0 +1,12 @@
+public class Chapter01 {
+  public static void main(String[] args) {
+    System.out.println("31+31の計算を始めます。");
+    System.out.println(31 + 31 + "です。");
+    int x;
+    x = 13;
+    System.out.println(x * x * 3.14);
+    int age;
+    age = 20;
+    System.out.println("私は" + age + "歳です。");
+  }
+}
