589d1b81a78ce8b10b18cb500cd23be3b73908b3
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-09 09:21:10 +0000

    記号表記

---
+++ Chapter05.java
@@ -0,0 +1,8 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    System.out.println("私の好きな記号は二重引用符 (\")です。");
+    // エスケープシーケンはバックスラッシュ（\）をつけて表記する。
+    
+    System.out.println("今の私の所持金は¥1200円です");
+  }
+}
