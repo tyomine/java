6ef164f39d1b19ae36148fa23e21d02e4ff367ff
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-09 12:56:16 +0000

    削除

--- Chapter04.java
+++ Chapter04.java
@@ -16,23 +16,3 @@ public class Chapter04 {
 		System.out.println(array[3]);
   }
 }
-
-public class Training01 {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 10;
-    String ans = "x+yは" + (x + y);
-    System.out.println(ans);
-    
-    System.out.println("ようこそ占いの館へ");
-    System.out.println("あなたの名前を入力してください。");
-    String name = new java.util.Scanner(System.in).nextLine();
-    System.out.println("あなたの年齢を入力してください。");
-    int age = new java.util.Scanner(System.in).nextInt;
-    int fortune = new java.util.Random().nextInt(3);
-    int number = (fortune + 1);
-    System.out.println("占い結果が出ました！");
-    System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + number + "です。")
-    System.out.println("1:大吉 2:中吉 3:吉 4:凶")
-  }
-}
