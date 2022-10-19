85151485d06b3a99ce06bb17a2e322d5195de289
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-19 11:08:48 +0000

    数当てゲーム

---
+++ Training011.java
@@ -0,0 +1,17 @@
+public class Training011 {
+  public static void main(String[] args) {
+    System.out.println("数当てゲーム");
+		int ans = new java.util.Random().nextInt(3);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0~9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("当たり！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+			System.out.println("ゲームを終了します");
+		}
+  }
+}
