b2b169e46bc15939d69a5aa902af5bc97e5fe04a
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-25 14:07:45 +0000

    java学習

--- Chapter02.java
+++ Chapter02.java
@@ -1,8 +1,17 @@
 public class Chapter02 {
-  public static void main(String[] args) {
-    int age = 20;
-    System.out.println("私の年齢は" + age);
-    age = 32;
-    System.out.println("...本当の年齢は" + age);
-  }
+	public static void main(String[] args) {
+		String address = "hoge@hoge";
+		String text = "私は元気ですよ";
+		email(address, text);
+	}
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました。");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
+	}
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました。");
+		System.out.println("件名" + title);
+		System.out.println("本文" + text);
+	}
 }
