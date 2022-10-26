ff90150b30ca870d0e85d74759a395e3cfd608b2
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-10-26 04:07:45 +0000

    java学習

---
+++ Sample02.java
@@ -0,0 +1,16 @@
+public class Sample02 {
+  public static void main(String[] args) {
+    introduceOneself();
+  }
+  
+  public static void introduceOneself() {
+    String name = "Kokkusan";
+    int age = 22;
+    double heigth = 169.9;
+    char like = '源';
+    System.out.println("私の名前は" + name + "です。");
+    System.out.println("歳は、" + age + "なのです");
+    System.out.println("身長は" + heigth + "cmでありんす");
+    System.out.println("好きな言葉は" + like + "でっし！");
+  }
+}
