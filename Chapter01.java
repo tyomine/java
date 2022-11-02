4c61139ad7b0d8df03e7837de4c61d263277751a
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-02 05:13:46 +0000

    java学習

--- Chapter01.java
+++ Chapter01.java
@@ -1,32 +1,21 @@
 public class Chapter01 {
   public static void main(String[] args) {
-    System.out.println("31+31の計算を始めます。");
-    System.out.println(31 + 31 + "です。");
-    int x;
-    x = 13;
+    System.out.println("三根くん");
+    System.out.println("26歳です。");
+    System.out.println("就職活動中です。");
+    System.out.println("31+31の計算をします。");
+    System.out.println(31 + 31);
+    int x = 6;
     System.out.println(x * x * 3.14);
-    int age;
-    age = 20;
-    System.out.println("私は" + age + "歳です。");
     
-    int version;
-    version = 1 + 5;
-    System.out.println("日記ソフト");
-    System.out.println("ver" + version);
-    System.out.println("まだ開発中です。");
-    System.out.println("終了します");
-    
-    int hoge;
-    hoge = 30;
-    System.out.println(hoge);
-    
-    boolean isError;
-    isError = true;
-    boolean  result;
-    result = false;
-    char zodiac;
-    zodiac = '壱';
-    String name; name = "すがわら";
-    System.out.println(zodiac + name);
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("横幅" + a + "縦幅" + b +"の長方形の面積は、" + c);
+    boolean ww = true;
+    char ee = '駆';
+    double qwe = 3.14;
+    long qwee = 1212312312312312312L;
+    String scac = "ミナトの攻撃！敵に１５ポイントのダメージを与えた。";
   }
 }
