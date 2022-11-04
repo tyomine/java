9b6490af5e8b7fe3d75c79f9525f46d76ba5fa9a
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-04 01:17:56 +0000

    学習

--- Chapter02.java
+++ Chapter02.java
@@ -1,17 +1,26 @@
 public class Chapter02 {
 	public static void main(String[] args) {
-		String address = "hoge@hoge";
-		String text = "私は元気ですよ";
-		email(address, text);
-	}
-	public static void email(String address, String text) {
-		System.out.println(address + "に、以下のメールを送信しました。");
-		System.out.println("件名：無題");
-		System.out.println("本文：" + text);
-	}
-	public static void email(String title, String address, String text) {
-		System.out.println(address + "に、以下のメールを送信しました。");
-		System.out.println("件名" + title);
-		System.out.println("本文" + text);
+		// エスケープシーケンス
+		System.out.println("私の好きな記号は(\")です。");
+		int a = 100;
+		a++;
+		System.out.println(a);
+		
+		int b = 5;
+		int c = 3;
+		int m = Math.max(b,c);
+		System.out.println("比較実験：" + b + "と" + c + "とで大きい方は・・・" + m);
+		
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたのは来年、" + (n + 1) + "歳になりますね。");
+		
+		int r = new java.util.Random().nextInt(30);
+		System.out.println("あなたの年齢は"+ r + "歳ですね！");
+		System.out.println("あなたの名前を入力してください。");
+		String z = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください。");
+		int g = new java.util.Scanner(System.in).nextInt();
+		System.out.println("ようこそ" + g + "歳の" + z + "さん！！");
 	}
 }
