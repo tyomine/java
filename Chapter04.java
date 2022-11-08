04cccde0955931ea1197ada7219ca8fb62ab0d37
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-08 11:06:06 +0000

    java学習

--- Chapter04.java
+++ Chapter04.java
@@ -1,18 +1,47 @@
 public class Chapter04 {
-  public static void main(String[] args) {
-    String string1;
-		string1 = "あいう";
-		System.out.println(string1 + "の文字数:" + string1.length());
-		string1 = "";
-		System.out.println(string1 + "の文字数:" + string1.length());
-
-		int[] array1 = new int[3];
-		array1[0] = 60;
-		array1[1] = 20;
-		array1[2] = 20;
-		System.out.println(array1[0]);
+	public static void main(String[] args) {
+		// 問題１
+		// int isHungry = 0;
+		// String food = "かめはめ波";
+		// System.out.println("こんにちわ");
+		// if (isHungry == 0){
+		// 	System.out.println("お腹がいっぱいです。");
+		// } else {
+		// 	System.out.println("はらぺこです。");
+		// 	System.out.println(food + "をいただきます。");
+		// 	System.out.println("ごちそうさまでした。");
+		// }
 		
-		int[] array = {0, 10, 20, 30, 40};
-		System.out.println(array[3]);
-  }
+		// 問題２
+		// System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+		// int selected = new java.util.Scanner(System.in).nextInt();
+		// switch (selected) {
+		// 	case 1:
+		// 		System.out.println("検索します。");
+		// 		break;
+		// 	case 2:
+		// 		System.out.println("登録します。");
+		// 		break;
+		// 	case 3:
+		// 		System.out.println("削除します。");
+		// 		break;
+		// 	case 4:
+		// 		System.out.println("変更します。");
+		// }
+		
+		// 問題３
+		System.out.print("数当てゲーム");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0~9の数字を入力してください。");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("当たり！");
+				break;
+			} else if (num != ans){
+				System.out.println("違います。");
+			}
+		}
+		System.out.println("ゲームを終了します。");
+	}
 }
