aa48396236c88785043411fdec26b2f8be0484b3
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-13 11:37:41 +0000

    学習

--- Chapter07.java
+++ Chapter07.java
@@ -1,45 +1,29 @@
 public class Chapter07 {
-  public static void main(String[] args) {
-    // 二種類のwhile文
-    int temp = 27;
-    while (temp > 25) {
-      temp--;
-      System.out.println("温度を1度下げました");
-    }
-    
-    // doを使用することで必ず最初に処理が行われる
-    int temp1 = 28;
-    do {
-      temp1--;
-      System.out.println("温度を一度下げました。");
-    } while (temp1 > 25);
-    
-    // for文による繰り返し
-    // 基本できなfor文
-    for (int i = 0; i < 4; i++) {
-       System.out.print("現在" + (i+1) +"周目↓");
-       System.out.println("");
-    }
-    
-    // printは文字列の表示後改行しない
-    // printlnは文字列の表示後に改行する
-    // printfメソッドは、文字列を指定した書式で画面に表示するメソッド
-    
-    // 九九の表を出力する
-    for (int i = 1; i < 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.print(i * j);
-        System.out.print(" ");/*空白を出力する*/
-      }
-      System.out.println("");/*改行を出力する*/
-    }
-    
-    // break文
-    for (int i = 1; i < 10; i++) {
-      if (i == 4) {
-        break;
-      }
-      System.out.println(i);
-    }
-  }
+	public static void main(String[] args) {
+		int kyori = 16;
+		if (kyori <= 5) {
+			System.out.println("とても近いです。");
+		} else if (kyori <= 10){
+			System.out.println("近いです。");
+		} else if (kyori <= 15){
+			System.out.println("遠いです。");
+		} else {
+			System.out.println("とても遠いです。");
+		}
+
+		String color = "blue";
+		switch (color) {
+		case "red":
+			System.out.println("赤信号です。");
+			break;
+		case "yellow":
+			System.out.println("黄信号です。");
+			break;
+		case "blue":
+			System.out.println("青信号です。");
+			break;
+		default:
+			System.out.println("信号の色ではありません。");
+		}
+	}
 }
