f8185a7e9424a0341b9d42ebb6b89e48f9f8e637
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-24 13:22:39 +0000

    学習

--- Chapter10.java
+++ Chapter10.java
@@ -1,27 +1,17 @@
-// オーバーロードの利用
-// 同じメソットを使うことは基本的にはできないが引数が異なる場合は使用できる。
 public class Chapter10 {
-  // 1つ目のaddメソッド
-  public static int add(int x, int y) {
-    return x + y;
-  }
-  //2つ目のメソッド
-  public static double add(double x, double y) {
-    return x + y;
-  }
-  //3つ目のメソッド
-  public static String add(String x, String y) {
-    return x + y;
-  }
-  //４つ目のメソッド
-  public static int add(int x, int y, int z) {
-    return x + y +z;
-  }
-  
-  public static void main(String[] args) {
-    System.out.println(add(10, 20));/*1つ目のメソッドが呼ばれる*/
-    System.out.println(add(3.5, 2.7));/*2つ目のメソッドが呼ばれる*/
-    System.out.println(add("Hello", "World"));/*3つ目のメソッドが呼ばれる*/
-    System.out.println("10+20+30=" + add(10,20,30));/*４つ目のメソッドが呼ばれる*/
-  }
+	public static void main(String[] args) {
+		System.out.println("数当てゲーム");
+		int ans = new java.util.Random().nextInt(3);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0~9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("当たり！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+			System.out.println("ゲームを終了します");
+		}
+	}
 }
