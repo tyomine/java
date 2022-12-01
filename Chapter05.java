7de9886917debd5169d0d4a5713a03d895fec4f4
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-12-01 16:10:02 +0000

    学習

--- Chapter05.java
+++ Chapter05.java
@@ -1,11 +1,17 @@
 public class Chapter05 {
-	public static void main(String[] args) {
-		long long1 = 12312312312123L;
-		double double1 = 3.14;
-		boolean boolean1 = true;
-		System.out.println(long1 + "," + double1 + "," + boolean1);
-
-		int[] array = {0, 10, 20, 30, 40};
-		System.out.println(array[3]);
-	}
+  public static void main(String[] args) {
+    System.out.println("数当てゲーム");
+		int ans = new java.util.Random().nextInt(3);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0~9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("当たり！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+			System.out.println("ゲームを終了します");
+		}
+  }
 }
