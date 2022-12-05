2634a78fab074f2b8a23962e18fb7637f4d4cc0e
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-12-05 11:35:28 +0000

    学習

--- Human01.java
+++ Human01.java
@@ -1,14 +1,32 @@
 public class Human01 {
-  public String name;
-  public int age;
-  
-  public Human01() {
-    name = "山田";
-    age = 20;
-  }
-  public Human01(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
+	public static void main(String[] args) {
+		System.out.println("こんにちわ！");
+		System.out.println("こんにちわ！");
+		System.out.println("31才です");
+		System.out.println("お酒が好きです。");
+		System.out.println("31+31の計算をします");
+		System.out.println(31+31);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+		int age; //年齢を入れる箱
+		age = 20;
+		System.out.println("私は" + age + "歳です");
+		
+		String papa = "水戸黄門";
+		System.out.println(papa);
+		papa = "おじさん";
+		System.out.println(papa);
+		int like = 2;
+		System.out.println("私の好きな数字は" + like + "でありまする。");
+		
+		long long1 = 12312312312123L;
+		double double1 = 3.14;
+		boolean boolean1 = true;
+		System.out.println(long1 + "," + double1 + "," + boolean1);
+
+		int[] array = {0, 10, 20, 30, 40};
+		System.out.println(array[3]);
+	}
 }
 
